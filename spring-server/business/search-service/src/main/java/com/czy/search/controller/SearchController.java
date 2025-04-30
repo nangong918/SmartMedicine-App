d7294e5baff6f50efd812f808686c24eeaac6cf4
czy! <1322596900@qq.com>
2025-04-30 18:15:16 +0800

    refactor(maven): 调整 search-service 项目结构和依赖
    - 将 search-service 从 base 模块移动到 business模块
    - 更新 pom.xml 文件以反映新的项目结构
    - 添加 SearchConstant 类用于存储搜索服务的常量
    - 创建 SearchController 类以处理搜索请求
    - 更新 maven 依赖树和服务依赖树图

---
+++ spring-server/business/search-service/src/main/java/com/czy/search/controller/SearchController.java
@@ -0,0 +1,23 @@
+package com.czy.search.controller;
+
+import com.czy.api.constant.search.SearchConstant;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 13225
+ * @date 2025/4/30 18:13
+ */
+@Slf4j
+@CrossOrigin(origins = "*") // 跨域
+@RequiredArgsConstructor // 自动注入@Autowired
+@RestController
+@RequestMapping(SearchConstant.MainSearch_CONTROLLER)
+public class SearchController {
+
+
+
+}
