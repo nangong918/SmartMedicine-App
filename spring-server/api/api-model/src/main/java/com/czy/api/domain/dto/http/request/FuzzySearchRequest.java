88794013d44cfb9ee1623bae22d67cc468d40475
czy! <1322596900@qq.com>
2025-05-05 09:36:17 +0800

    feat(search): 优化模糊搜索功能并添加数据校验
    - 在 FuzzySearchRequest 类中为 userAccount 和 sentence 字段添加了数据校验注解
    - 更新了搜索流程图，将 Bert NLJ 和 NLL 模型应用于自然语言检查和合法检查
    -优化了四级搜索的描述，使用了更准确的术语

--- spring-server/api/api-model/src/main/java/com/czy/api/domain/dto/http/request/FuzzySearchRequest.java
+++ spring-server/api/api-model/src/main/java/com/czy/api/domain/dto/http/request/FuzzySearchRequest.java
@@ -3,6 +3,9 @@ package com.czy.api.domain.dto.http.request;
 import json.BaseBean;
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 /**
  * @author 13225
  * @date 2025/4/30 18:27
@@ -10,7 +13,10 @@ import lombok.Data;
 @Data
 public class FuzzySearchRequest implements BaseBean {
     // 用户account，用于搜索的context特征上下文
+    @NotEmpty(message = "发送者账号不能为空")
     public String userAccount;
-    // 搜索的句子
+    // 搜索的句子 最大长度15，最小长度2
+    @NotEmpty(message = "搜索的句子不能为空")
+    @Size(min = 2, max = 15, message = "搜索的句子长度必须在2-15之间")
     public String sentence;
 }
