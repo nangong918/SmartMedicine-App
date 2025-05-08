518cd7901338f3b4b411ad4b01aa1fe23d86b179
czy! <1322596900@qq.com>
2025-05-08 18:31:00 +0800

    feat(feature-service): 迁移特征service

---
+++ spring-server/business/feature-service/src/main/java/com/czy/feature/service/PostFeatureService.java
@@ -0,0 +1,9 @@
+package com.czy.feature.service;
+
+/**
+ * @author 13225
+ * @date 2025/5/8 18:21
+ * 1. 发布的时候用户手动打分区标签 + Bert模型对文章进行分类：#日常分享 #专业医疗知识 #养生技巧 #医疗新闻 #其他
+ */
+public interface PostFeatureService {
+}
