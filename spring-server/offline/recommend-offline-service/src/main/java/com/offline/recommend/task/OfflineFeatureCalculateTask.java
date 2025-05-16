55f690ff1784301200fc72ef0854a4ac20428b7c
czy! <1322596900@qq.com>
2025-05-16 11:15:51 +0800

    feat(recommend): 优化用户热度计算和排序
    - 添加用户热度数据空检查，避免无效计算- 实现用户热度列表排序功能，按热度值从高到低排序
    - 增加日志输出，记录用户数量和排序耗时
    - 优化代码结构，分离用户热度获取和处理逻辑

--- spring-server/offline/recommend-offline-service/src/main/java/com/offline/recommend/task/OfflineFeatureCalculateTask.java
+++ spring-server/offline/recommend-offline-service/src/main/java/com/offline/recommend/task/OfflineFeatureCalculateTask.java
@@ -8,8 +8,10 @@ import org.apache.dubbo.config.annotation.Reference;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -32,10 +34,33 @@ public class OfflineFeatureCalculateTask {
     @Scheduled(cron = CRON_EXPRESSION)
     public void offlineFeatureCalculate() {
         log.info("开始计算离线特征, 当前时间：{}", LocalDateTime.now());
+
+        List<UserHeatAo> userHeatAos = getUserHeatAos();
+        if (CollectionUtils.isEmpty(userHeatAos)){
+            log.warn("用户热度列表为空，结束计算");
+            return;
+        }
+
+    }
+
+    private List<UserHeatAo> getUserHeatAos() {
         long userHeatCalculateStartTime = System.currentTimeMillis();
         List<UserHeatAo> userHeatAos = userHeatService.getUsersHeat();
-        long userHeatCalculateEndTime = System.currentTimeMillis();
-        log.info("计算用户热度完成，耗时：{}ms, 当前时间：{}", userHeatCalculateEndTime - userHeatCalculateStartTime, LocalDateTime.now());
+        if (CollectionUtils.isEmpty(userHeatAos)){
+            return new ArrayList<>();
+        }
+        else {
+            log.info("用户数量：{}", userHeatAos.size());
+        }
+        // 按照热度从大到校排序 + 排序时间
+        log.info("开始排序用户热度列表");
+        long userHeatSortStartTime = System.currentTimeMillis();
+        userHeatAos.sort((o1, o2) -> o2.getHeatScore().compareTo(o1.getHeatScore()));
+        // 输出排序时间和计算时间
+        log.info("排序耗时：{}ms， 计算总耗时：{}ms",
+                System.currentTimeMillis() - userHeatSortStartTime,
+                System.currentTimeMillis() - userHeatCalculateStartTime);
+        return userHeatAos;
     }
 
 }
