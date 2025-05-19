7f9e93bf2047d068e3ffa034ab7c2a7fb99d4d50
czy! <1322596900@qq.com>
2025-05-19 14:12:49 +0800

    docs(recommend-offline-service): 图召回方案文档

--- spring-server/offline/recommend-offline-service/src/main/java/com/offline/recommend/service/impl/DistributedOfflineRecallCalculateServiceImpl.java
+++ spring-server/offline/recommend-offline-service/src/main/java/com/offline/recommend/service/impl/DistributedOfflineRecallCalculateServiceImpl.java
@@ -19,12 +19,24 @@ public class DistributedOfflineRecallCalculateServiceImpl implements Distributed
 
     private final UserFeatureRepository userFeatureRepository;
 
+    /**
+     * 图召回
+     * 1.user画像构建：entity（itemCF：entity - post的映射关系 + [post分类标签]）
+     * 2.知识工程：entity - relation - entity的映射关系
+     * 3.图相似度：entity - similarEntity：相似实体映射关系 (此相似度存在偏差，最好只选用top2)
+     * 4.user关系图：user和user的关系图谱 （最相似的好友，swing:user-entity/post-user趋同推荐）：关系userCF（不用user-CF，user-CF不稳定）
+     * @param userId
+     * @return
+     */
     @Override
     public List<Long> graphRecall(Long userId) {
-        // 路径1就是临时特征，不参与
-        // 共同邻居：user->entity：对不同的entity单独写mapper函数，因为每个entity的关系不同。主要关心的是：疾病，症状，药品，食物，菜谱 TODO 存在问题，user-entity的关系本来就很少交集，共同邻居应该是相同的实体
-        // Jaacard相似度
-        // 了解更多的图相似度计算
+        // 1. user 画像构建 -> （带有权重的entity集合）
+
+        // 2. 知识工程：(entity - relation - entity) -> 包含关联性(共同邻居)的entity集合
+
+        // 3. 图相似度：entity - similarEntity -> 包含相似度的有序集合
+
+        // 4. user关系图：swing图userCF -> 相似的user画像集合 -> （带有权重的entity集合）
         return null;
     }
 }
