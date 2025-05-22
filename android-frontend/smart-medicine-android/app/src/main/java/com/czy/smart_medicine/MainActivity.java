b52daa4baa823ed783ff6ed1eb15184aa6d181a5
czy! <1322596900@qq.com>
2025-05-22 15:48:29 +0800

    refactor(app): 重构 MainActivity 类
    - 将 AppCompatActivity 替换为 BaseActivity
    - 添加泛型类型 ActivityMainBinding
    - 移除 onCreate 方法，使用 init 方法替代- 优化类构造函数
    - 删除未使用的注释

--- android-frontend/smart-medicine-android/app/src/main/java/com/czy/smart_medicine/MainActivity.java
+++ android-frontend/smart-medicine-android/app/src/main/java/com/czy/smart_medicine/MainActivity.java
@@ -1,36 +1,34 @@
 package com.czy.smart_medicine;
 
-import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 import android.widget.TextView;
 
+import com.czy.baseUtilsLib.activity.BaseActivity;
 import com.czy.smart_medicine.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * @author 13225
+ */
+public class MainActivity extends BaseActivity<ActivityMainBinding> {
 
-    // Used to load the 'smart_medicine' library on application startup.
     static {
         System.loadLibrary("smart_medicine");
     }
 
-    private ActivityMainBinding binding;
+
+    public MainActivity() {
+        super(MainActivity.class);
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+    protected void init() {
+        super.init();
 
         // Example of a call to a native method
         TextView tv = binding.sampleText;
         tv.setText(stringFromJNI());
     }
 
-    /**
-     * A native method that is implemented by the 'smart_medicine' native library,
-     * which is packaged with this application.
-     */
+
     public native String stringFromJNI();
 }
