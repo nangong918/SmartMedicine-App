b9a7d8efe6d535f95af6bd911be4119d228ce371
czy! <1322596900@qq.com>
2025-05-22 15:11:07 +0800

    feat(android): 初始化cpp android

--- android-frontend/smart-medicine-android/app/src/main/java/com/czy/smart_medicine/MainActivity.java
+++ android-frontend/smart-medicine-android/app/src/main/java/com/czy/smart_medicine/MainActivity.java
@@ -1,24 +1,36 @@
 package com.czy.smart_medicine;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.czy.smart_medicine.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'smart_medicine' library on application startup.
+    static {
+        System.loadLibrary("smart_medicine");
+    }
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Example of a call to a native method
+        TextView tv = binding.sampleText;
+        tv.setText(stringFromJNI());
     }
+
+    /**
+     * A native method that is implemented by the 'smart_medicine' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
 }
