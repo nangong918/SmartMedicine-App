3e3e2cd02667397bbed88e33120999c03c2bc5fd
czy! <1322596900@qq.com>
2025-06-11 17:52:07 +0800

    feat: 优化上传结构

--- spring-server/business/user-relationship-service/src/main/java/com/czy/user/controller/UserFileController.java
+++ spring-server/business/user-relationship-service/src/main/java/com/czy/user/controller/UserFileController.java
@@ -61,7 +61,71 @@ public class UserFileController {
             @RequestParam("phone") String phone,
             @RequestParam("userId") Long userId
     ) {
-       return handleUpload(img, phone, userId, OssTaskTypeEnum.ADD.getCode());
+        if (img == null){
+            return BaseResponse.LogBackError("请上传图片");
+        }
+        if (!StringUtils.hasText(phone)){
+            return BaseResponse.LogBackError("请输入手机号");
+        }
+        if (userId == null){
+            return BaseResponse.LogBackError("请输入用户id");
+        }
+
+        String userImageBucket = UserConstant.USER_FILE_BUCKET + userId;
+        String ossKey = UserConstant.USER_REGISTER_REDIS_KEY + phone;
+        String lockPath = UserConstant.Login_CONTROLLER + UserConstant.Password_Register;
+
+        String errorMsg = String.format("请检查phone：%s 是否申请注册", phone);
+        if (!redissonService.hasKey(ossKey)) {
+            log.warn("用户上传头像的锁不存在, userPhone: {}", phone);
+            releaseLock(phone, lockPath);
+
+            return BaseResponse.LogBackError(errorMsg);
+        }
+
+        List<Long> fileIdList = new ArrayList<>();
+
+        try {
+            List<MultipartFile> multipartFiles = new ArrayList<>(1);
+            multipartFiles.add(img);
+            FileOptionResult fileOptionResult = minIOService.uploadFiles(
+                    multipartFiles, userId, userImageBucket);
+            ossService.uploadFilesRecord(fileOptionResult.getSuccessFiles(), userId, userImageBucket);
+            List<Long> successIds = fileOptionResult.getSuccessFiles()
+                    .stream()
+                    .map(SuccessFile::getFileId)
+                    .collect(Collectors.toList());
+
+            fileIdList.addAll(successIds);
+
+            if (!fileIdList.isEmpty()){
+                UserOssResponse userOssResponse = new UserOssResponse();
+                userOssResponse.setUserId(userId);
+                userOssResponse.setPhone(phone);
+                userOssResponse.setFileIds(fileIdList);
+                userOssResponse.setClusterLockPath(lockPath);
+                userOssResponse.setFileRedisKey(ossKey);
+                userOssResponse.setOssResponseType(OssResponseTypeEnum.SUCCESS.getCode());
+                userOssResponse.setOssOperationType(OssTaskTypeEnum.ADD.getCode());
+
+                boolean result = finishRegister(userOssResponse);
+                if (!result){
+                    log.warn("处理UserOssResponseEvent失败, userPhone: {}", phone);
+                    return BaseResponse.LogBackError(errorMsg);
+                }
+                else {
+                    // 删除redis缓存
+                    redissonService.deleteObject(ossKey);
+                }
+                return BaseResponse.getResponseEntitySuccess("上传成功");
+            }
+            else {
+                return BaseResponse.LogBackError(errorMsg);
+            }
+        } catch (Exception e){
+            releaseLock(phone, lockPath);
+            return BaseResponse.LogBackError(errorMsg);
+        }
     }
 
     // 修改头像
@@ -185,92 +249,7 @@ public class UserFileController {
         return BaseResponse.LogBackError(errorMsg);
     }
 
-    private BaseResponse<String> handleUpload(
-            MultipartFile img,
-            String phone,
-            Long userId,
-            Integer operationType
-    ){
-        if (img == null){
-            return BaseResponse.LogBackError("请上传图片");
-        }
-        if (!StringUtils.hasText(phone)){
-            return BaseResponse.LogBackError("请输入手机号");
-        }
-        if (userId == null){
-            return BaseResponse.LogBackError("请输入用户id");
-        }
-
-        String userImageBucket = UserConstant.USER_FILE_BUCKET + userId;
-        String ossKey = UserConstant.USER_REGISTER_REDIS_KEY + phone;
-        String lockPath = UserConstant.Login_CONTROLLER + UserConstant.Password_Register;
-
-        String errorMsg = String.format("请检查phone：%s 是否申请注册", phone);
-        if (!redissonService.hasKey(ossKey)) {
-            log.warn("用户上传头像的锁不存在, userPhone: {}", phone);
-            releaseLock(phone, lockPath);
-
-            return BaseResponse.LogBackError(errorMsg);
-        }
-
-        // 幂等
-        List<Long> fileIdList = new ArrayList<>();
-        List<MultipartFile> files = new ArrayList<>(1);
-        files.add(img);
-        files.removeIf(file -> {
-            String fileName = file.getOriginalFilename();
-            Long fileSize = file.getSize();
-            FileIsExistResult result = ossService.checkFileNameExistForResult(userId, fileName, userImageBucket, fileSize);
-            if (result.getIsExist()) {
-                fileIdList.add(result.getFileId());
-                return true; // 移除已存在的文件
-            }
-            return false; // 保留文件
-        });
-
-        try {
-            List<MultipartFile> multipartFiles = new ArrayList<>(1);
-            multipartFiles.add(img);
-            FileOptionResult fileOptionResult = minIOService.uploadFiles(
-                    multipartFiles, userId, userImageBucket);
-            ossService.uploadFilesRecord(fileOptionResult.getSuccessFiles(), userId, userImageBucket);
-            List<Long> successIds = fileOptionResult.getSuccessFiles()
-                    .stream()
-                    .map(SuccessFile::getFileId)
-                    .collect(Collectors.toList());
-
-            fileIdList.addAll(successIds);
-
-            if (!fileIdList.isEmpty()){
-                UserOssResponse userOssResponse = new UserOssResponse();
-                userOssResponse.setUserId(userId);
-                userOssResponse.setPhone(phone);
-                userOssResponse.setFileIds(fileIdList);
-                userOssResponse.setClusterLockPath(lockPath);
-                userOssResponse.setFileRedisKey(ossKey);
-                userOssResponse.setOssResponseType(OssResponseTypeEnum.SUCCESS.getCode());
-                userOssResponse.setOssOperationType(operationType);
-
-                boolean result = finishRegister(userOssResponse);
-                if (!result){
-                    log.warn("处理UserOssResponseEvent失败, userPhone: {}", phone);
-                    return BaseResponse.LogBackError(errorMsg);
-                }
-                else {
-                    // 删除redis缓存
-                    redissonService.deleteObject(ossKey);
-                }
-                return BaseResponse.getResponseEntitySuccess("上传成功");
-            }
-            else {
-                return BaseResponse.LogBackError(errorMsg);
-            }
-        } catch (Exception e){
-            releaseLock(phone, lockPath);
-            return BaseResponse.LogBackError(errorMsg);
-        }
-    }
-
+    // 注册
     private boolean finishRegister(UserOssResponse userOssResponse){
         String clusterLockPath = userOssResponse.getClusterLockPath();
         String phone = userOssResponse.getPhone();
@@ -300,11 +279,11 @@ public class UserFileController {
                         loginService.registerStorageToDatabase(loginUserDo);
                         return true;
                     }
-                    // 修改
-                    else if (OssTaskTypeEnum.UPDATE.getCode() == userOssResponse.getOssOperationType()){
-                        loginService.updateStorageToDatabase(loginUserDo);
-                        return true;
-                    }
+//                    // 修改
+//                    else if (OssTaskTypeEnum.UPDATE.getCode() == userOssResponse.getOssOperationType()){
+//                        loginService.updateStorageToDatabase(loginUserDo);
+//                        return true;
+//                    }
                 } catch (Exception e){
                     log.error("获取redis失败，fileRedisKey: {}", fileRedisKey);
                 }
@@ -321,6 +300,7 @@ public class UserFileController {
         return false;
     }
 
+    // 更新
     private boolean finishUpdate(UserOssResponse userOssResponse){
         try {
             LoginUserDo loginUserDo = loginUserMapper.getLoginUser(userOssResponse.getUserId());
