86bba6a8e508e1339dd64536d9ad0d86b1f8efb6
czy! <1322596900@qq.com>
2025-07-24 13:46:34 +0800

    refactor(db): 更新数据库表结构
    - 修改 login_user 表，将 id 设置为自动增量
    - 更新 oss_file 表，将 user_id 设置为非空字段
    - 调整 post_files 表，移除自动增量属性
    - 更新多个表的引擎参数和字符集
    - 删除 test_search 和 user_health_data 表

--- spring-server/db/smart_medicine.sql
+++ spring-server/db/smart_medicine.sql
@@ -1,17 +1,17 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : localhost_3306
+ Source Server         : localhost
  Source Server Type    : MySQL
- Source Server Version : 80034
+ Source Server Version : 80042
  Source Host           : localhost:3306
  Source Schema         : smart_medicine
 
  Target Server Type    : MySQL
- Target Server Version : 80034
+ Target Server Version : 80042
  File Encoding         : 65001
 
- Date: 05/06/2025 16:15:04
+ Date: 24/07/2025 13:44:57
 */
 
 SET NAMES utf8mb4;
@@ -42,36 +42,36 @@ CREATE TABLE `friend_apply`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `login_user`;
 CREATE TABLE `login_user`  (
-  `id` bigint NOT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `user_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `account` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT 'account和phone至少一个不为null',
   `password` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT 'account非null的时候不为null',
   `phone` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT 'account和phone至少一个不为null',
   `permission` int NOT NULL COMMENT '用户的权限等级：user：1，admin：2',
-  `avatar_file_id` bigint NULL DEFAULT NULL COMMENT '用户的头像链接',
+  `avatar_file_id` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '用户的头像链接',
   `register_time` bigint NOT NULL,
   `last_online_time` bigint NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `uniq_account`(`account` ASC) USING BTREE,
   INDEX `normal_phone`(`phone` ASC) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 1947602063380209665 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Table structure for oss_file
 -- ----------------------------
 DROP TABLE IF EXISTS `oss_file`;
 CREATE TABLE `oss_file`  (
-  `id` bigint NOT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `file_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `upload_timestamp` bigint NOT NULL,
-  `user_id` bigint NULL DEFAULT NULL,
+  `user_id` bigint NOT NULL,
   `file_storage_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `file_size` bigint NOT NULL,
   `bucket_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `idx_user_id`(`user_id` ASC) USING BTREE,
   INDEX `idx_bucket_storage`(`bucket_name` ASC, `file_storage_name` ASC) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 305 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 1947602066018426881 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Table structure for post_collect
@@ -83,7 +83,7 @@ CREATE TABLE `post_collect`  (
   `collect_folder_id` bigint NOT NULL,
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `idx_collect_folder`(`collect_folder_id` ASC) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Table structure for post_collect_folder
@@ -94,21 +94,22 @@ CREATE TABLE `post_collect_folder`  (
   `post_id` bigint NOT NULL,
   `user_id` bigint NOT NULL,
   `collect_folder_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `collect_folder_id` bigint NOT NULL,
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `unique_post_user`(`post_id` ASC, `user_id` ASC) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Table structure for post_files
 -- ----------------------------
 DROP TABLE IF EXISTS `post_files`;
 CREATE TABLE `post_files`  (
-  `id` bigint NOT NULL AUTO_INCREMENT,
+  `id` bigint NOT NULL,
   `post_id` bigint NOT NULL,
   `file_id` bigint NOT NULL,
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `idx_post_id`(`post_id` ASC) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1930534728920539137 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Table structure for post_info
@@ -122,10 +123,9 @@ CREATE TABLE `post_info`  (
   `collect_count` bigint NOT NULL,
   `comment_count` bigint NOT NULL,
   `forward_count` bigint NULL DEFAULT 0,
-  `title` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `idx_author_id`(`author_id` ASC) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1930534728484331521 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Table structure for session
@@ -147,17 +147,7 @@ CREATE TABLE `session`  (
   `type` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `idx_uid`(`uid` ASC) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 383 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
-
--- ----------------------------
--- Table structure for test_search
--- ----------------------------
-DROP TABLE IF EXISTS `test_search`;
-CREATE TABLE `test_search`  (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `search_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Table structure for user_chat_message
@@ -175,7 +165,7 @@ CREATE TABLE `user_chat_message`  (
   INDEX `idx_receiver`(`receiver_id` ASC) USING BTREE,
   INDEX `idx_sender_receiver`(`sender_id` ASC, `receiver_id` ASC) USING BTREE,
   INDEX `idx_timestamp`(`timestamp` ASC) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 185 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Table structure for user_friend
@@ -192,37 +182,6 @@ CREATE TABLE `user_friend`  (
   INDEX `idx_user_id`(`user_id` ASC) USING BTREE,
   INDEX `idx_friend_id`(`friend_id` ASC) USING BTREE,
   INDEX `idx_timestamp`(`add_time` ASC) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
-
--- ----------------------------
--- Table structure for user_health_data
--- ----------------------------
-DROP TABLE IF EXISTS `user_health_data`;
-CREATE TABLE `user_health_data`  (
-  `id` bigint NOT NULL,
-  `user_id` bigint NOT NULL,
-  `time` bigint NOT NULL,
-  `hypertension` tinyint(1) NULL DEFAULT NULL,
-  `high_cholesterol` tinyint(1) NULL DEFAULT NULL,
-  `bmi` tinyint(1) NULL DEFAULT NULL,
-  `smoking` tinyint(1) NULL DEFAULT NULL,
-  `stroke` tinyint(1) NULL DEFAULT NULL,
-  `physical_activity` tinyint(1) NULL DEFAULT NULL,
-  `fruit_consumption` tinyint(1) NULL DEFAULT NULL,
-  `vegetable_consumption` tinyint(1) NULL DEFAULT NULL,
-  `heavy_drinking` tinyint(1) NULL DEFAULT NULL,
-  `any_healthcare` tinyint(1) NULL DEFAULT NULL,
-  `no_medical_expense` tinyint(1) NULL DEFAULT NULL,
-  `general_health_status` tinyint(1) NULL DEFAULT NULL,
-  `mental_health` tinyint(1) NULL DEFAULT NULL,
-  `physical_health` tinyint(1) NULL DEFAULT NULL,
-  `walking_difficulty` tinyint(1) NULL DEFAULT NULL,
-  `gender` tinyint(1) NULL DEFAULT NULL,
-  `age` int NULL DEFAULT NULL,
-  `education_level` tinyint(1) NULL DEFAULT NULL,
-  `income` int NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `user_id`(`user_id` ASC) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 SET FOREIGN_KEY_CHECKS = 1;
