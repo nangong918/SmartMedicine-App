6dba8feb375711863f21cf8e0ed00ac1f30de337
chenzy <1322596900@qq.com>
2025-08-06 11:00:56 +0800

    feat(recommend): 完善离线推荐功能并记录推荐数据- 在 OfflineRecommendServiceImpl 中添加 RecommendedRecordsService 依赖 - 实现 getOfflineRecommend 方法，从 Redis 中获取推荐帖子 - 新增记录推荐数据的功能，将推荐过的帖子 ID 存储到推荐记录中 - 优化代码结构，提高可读性和可维护性

--- spring-server/offline/recommend-offline-service/src/main/java/com/offline/recommend/service/impl/OfflineRecommendServiceImpl.java
+++ spring-server/offline/recommend-offline-service/src/main/java/com/offline/recommend/service/impl/OfflineRecommendServiceImpl.java
@@ -5,6 +5,7 @@ import com.czy.api.api.offline.OfflineRecommendService;
 import com.czy.api.constant.feature.FeatureConstant;
 import com.czy.api.constant.offline.OfflineRedisConstant;
 import com.czy.api.domain.ao.recommend.PostScoreAo;
+import com.offline.recommend.service.RecommendedRecordsService;
 import com.utils.mvc.redisson.RedissonService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,6 +14,7 @@ import org.springframework.util.CollectionUtils;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author 13225
@@ -25,21 +27,41 @@ import java.util.List;
 public class OfflineRecommendServiceImpl implements OfflineRecommendService {
 
     private final RedissonService redissonService;
+    private final RecommendedRecordsService recommendedRecordsService;
 
+    /**
+     * 获取离线推荐
+     * @param userId    用户ID
+     * @return          推荐帖子ids
+     */
     @Override
     public List<PostScoreAo> getOfflineRecommend(Long userId) {
+        // 获取缓存在redis的推荐帖子id
         String userRecommendKey = OfflineRedisConstant.USER_RECOMMEND_KEY + ":" + userId;
+
+        // 获取离线层计算出来的帖子
         if (redissonService.zCount(userRecommendKey) > 0){
-            List<Object> recommendPostScoreAos = redissonService.zPopTopNAndRemove(userRecommendKey, FeatureConstant.USER_RECOMMEND_GET_NUM);
+            // redis中取出指定数量的post, 并且从未推荐的post记录中删除
+            List<Object> recommendPostScoreAos = redissonService.zPopTopNAndRemove(
+                    userRecommendKey, FeatureConstant.USER_RECOMMEND_GET_NUM
+            );
+
             List<PostScoreAo> postScoreAoList = new ArrayList<>();
             if (CollectionUtils.isEmpty(recommendPostScoreAos)){
                 return new ArrayList<>();
             }
+
             for (Object recommendPostScoreAo : recommendPostScoreAos){
                 if (recommendPostScoreAo instanceof PostScoreAo){
                     postScoreAoList.add((PostScoreAo) recommendPostScoreAo);
                 }
             }
+
+            // 记录推荐过的数据
+            List<Long> postIds = postScoreAoList.stream()
+                    .map(PostScoreAo::getPostId)
+                    .collect(Collectors.toList());
+            recommendedRecordsService.recordRecommendedPosts(userId, postIds);
             return postScoreAoList;
         }
         return new ArrayList<>();
