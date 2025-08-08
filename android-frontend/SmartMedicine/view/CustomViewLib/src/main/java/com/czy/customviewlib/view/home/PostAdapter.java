f3bd105bc4441fa56e523ead012a51122b6e8868
chenzy <1322596900@qq.com>
2025-08-08 14:41:06 +0800

    fix(android-recommend): 修复卡片类型错误bug

--- android-frontend/SmartMedicine/view/CustomViewLib/src/main/java/com/czy/customviewlib/view/home/PostAdapter.java
+++ android-frontend/SmartMedicine/view/CustomViewLib/src/main/java/com/czy/customviewlib/view/home/PostAdapter.java
@@ -1,5 +1,6 @@
 package com.czy.customviewlib.view.home;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
@@ -8,7 +9,6 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.czy.customviewlib.databinding.ViewRecommendCardBinding;
 import com.czy.customviewlib.databinding.ViewRecommendCardPlusBinding;
-import com.czy.dal.OnPositionItemClick;
 import com.czy.dal.ao.home.PostAo;
 import com.czy.dal.constant.home.RecommendCardType;
 
@@ -16,6 +16,8 @@ import java.util.List;
 import java.util.Optional;
 
 public class PostAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private final static String TAG = PostAdapter.class.getName();
 
     private final List<PostAo> postAoList;
     private final OnRecommendCardClick onRecommendCardClick;
@@ -30,13 +32,22 @@ public class PostAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-        if (viewType == RecommendCardType.TWO_SMALL_CARD.value) {
-            ViewRecommendCardBinding binding = ViewRecommendCardBinding.inflate(inflater, parent, false);
-            return new PostItemViewHolder(binding, onRecommendCardClick);
-        }
-        else{
-            ViewRecommendCardPlusBinding binding = ViewRecommendCardPlusBinding.inflate(inflater, parent, false);
-            return new PostItemPlusViewHolder(binding, onRecommendCardClick);
+        RecommendCardType recommendCardType = RecommendCardType.valueOf(viewType);
+        switch (recommendCardType){
+            // 大的卡片
+            case SINGLE_BIG_CARD -> {
+                ViewRecommendCardPlusBinding binding = ViewRecommendCardPlusBinding.inflate(inflater, parent, false);
+                return new PostItemPlusViewHolder(binding, onRecommendCardClick);
+            }
+            // 两个小卡片
+            case TWO_SMALL_CARD -> {
+                ViewRecommendCardBinding binding = ViewRecommendCardBinding.inflate(inflater, parent, false);
+                return new PostItemViewHolder(binding, onRecommendCardClick);
+            }
+            default -> {
+                Log.w(TAG, "未知的推荐卡片类型");
+                throw new IllegalArgumentException("不支持的推荐卡片类型");
+            }
         }
     }
 
@@ -46,11 +57,11 @@ public class PostAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
         if (postAo == null){
             return;
         }
-        if (postAo.viewType == RecommendCardType.TWO_SMALL_CARD.value){
-            ((PostItemViewHolder)holder).setView();
-        }
-        else {
-            ((PostItemPlusViewHolder)holder).setView();
+        RecommendCardType recommendCardType = RecommendCardType.valueOf(postAo.viewType);
+        switch (recommendCardType){
+            case SINGLE_BIG_CARD -> ((PostItemPlusViewHolder)holder).setView();
+            case TWO_SMALL_CARD -> ((PostItemViewHolder)holder).setView();
+            default -> Log.w(TAG, "未知的推荐卡片类型");
         }
     }
 
