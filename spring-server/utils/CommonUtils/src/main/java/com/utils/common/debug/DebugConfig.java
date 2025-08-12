1aeeaf631112fab705a9df0fa809b598cf43dc58
chenzy <1322596900@qq.com>
2025-08-12 17:18:47 +0800

    feat(utils): 添加 CommonUtils 模块
    - 新增 CommonUtils模块，包含多个实用工具类
    - 添加国际化、加密解密、IP 获取、JSON 处理等工具类
    - 新增线程池和调度器配置类- 添加调试配置类和端口自动管理初始化器

---
+++ spring-server/utils/CommonUtils/src/main/java/com/utils/common/debug/DebugConfig.java
@@ -0,0 +1,35 @@
+package com.utils.common.debug;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author 13225
+ * @date 2025/1/4 16:40
+ */
+@Setter
+@Getter
+@Slf4j
+@Component
+@ConfigurationProperties(prefix = "debug")
+public class DebugConfig {
+    private boolean debug = false;
+    private boolean allLog = false;
+    private boolean vcodeCheck = true;
+    private boolean accessTokenCheck = true;
+    private boolean recordUserAccount = true;
+
+    @Override
+    public String toString() {
+        return "DebugConfig{" +
+                "debug=" + debug +
+                ", allLog=" + allLog +
+                ", vcodeCheck=" + vcodeCheck +
+                ", accessTokenCheck=" + accessTokenCheck +
+                ", recordUserAccount=" + recordUserAccount +
+                '}';
+    }
+}
