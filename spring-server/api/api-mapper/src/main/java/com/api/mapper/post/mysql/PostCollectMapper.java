9674ce539cbe496ba519a62e14e5b0d289ba0d53
chenzy <1322596900@qq.com>
2025-08-13 11:38:18 +0800

    feat(api): 新增帖子相关 Mapper 和 DTO
    - 新增 PostDetailEsMapper、PostCommentMongoMapper、PostDetailMongoMapper 等 Mapper 类
    - 新增 PostCollectFolderMapper、PostCollectMapper、PostFilesMapper、PostInfoMapper 等 MySQL Mapper 接口
    - 新增对应的 XML 配置文件- 移除 protobuf 相关依赖
    - 添加 mybatis-plus依赖

---
+++ spring-server/api/api-mapper/src/main/java/com/api/mapper/post/mysql/PostCollectMapper.java
@@ -0,0 +1,61 @@
+package com.api.mapper.post.mysql;
+
+import com.czy.api.domain.Do.post.collect.PostCollectDo;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * @author 13225
+ * @date 2025/4/23 17:33
+ */
+@Mapper
+public interface PostCollectMapper {
+    // 通过id获取
+    PostCollectDo findPostCollectById(Long id);
+    // 通过postId和FolderId查找到
+    PostCollectDo findPostCollectByPostIdAndFolderId(
+            @Param("postId") Long postId,
+            @Param("collectFolderId") Long collectFolderId
+    );
+
+    // 通过idList获取
+    List<PostCollectDo> findPostCollectsByIdList(List<Long> idList);
+
+    // 通过collectFolderId获取全部List<PostCollectDo>
+    List<PostCollectDo> findPostCollectsByCollectFolderId(Long collectFolderId);
+
+    // 通过userId获取全部List<PostCollectDo> (需要联表查询)
+    List<PostCollectDo> findPostCollectsByUserId(Long userId);
+
+    /**
+     * 通过userId分页获取全部List<PostCollectDo> (需要联表查询)
+     * @param userId    用户id
+     * @param pageNum   页码
+     * @param pageSize  每页数量
+     * @return          List<PostCollectDo>
+     */
+    List<PostCollectDo> findPostCollectsByUserIdPaging(
+            @Param("userId") Long userId,
+            @Param("offset") int offset,
+            @Param("pageNum") int pageNum,
+            @Param("pageSize") int pageSize
+    );
+
+    // 增加PostCollectDo
+    void savePostCollect(PostCollectDo postCollectDo);
+
+    // 通过id删除
+    void deletePostCollect(Long id);
+
+    // 通过collectFolderId删除全部
+    void deletePostCollectsByCollectFolderId(Long collectFolderId);
+    // 联合postId和collectFolderId删除某条
+    void deletePostCollectByPostIdAndCollectFolderId(
+            @Param("postId") Long postId,
+            @Param("collectFolderId") Long collectFolderId);
+
+    // 更新PostCollectDo
+    void updatePostCollect(PostCollectDo postCollectDo);
+}
