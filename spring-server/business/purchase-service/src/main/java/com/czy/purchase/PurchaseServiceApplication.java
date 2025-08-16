9de49cc3071d172c08828a819a2a7d243bb7bbc0
chenzy <1322596900@qq.com>
2025-08-16 18:20:54 +0800

    feat(business): 添加 purchase-service模块
    - 在 business 模块中添加 purchase-service 子模块
    - 新增 PurchaseServiceApplication启动类
    - 添加 application.yml 配置文件
    - 创建 purchase-service 的 pom.xml 文件，包含所需依赖

---
+++ spring-server/business/purchase-service/src/main/java/com/czy/purchase/PurchaseServiceApplication.java
@@ -0,0 +1,33 @@
+package com.czy.purchase;
+
+
+import com.utils.common.start.PortApplicationContextInitializer;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+
+@MapperScan({"com.czy.purchase.mapper", "com.utils.minio.mapper"})    // 扫描mapper
+@SpringBootApplication(scanBasePackages = {
+        // 扫描api模块
+        "com.czy.api",
+        // 扫描本模块
+        "com.czy.purchase",
+        // 扫描工具类
+        "com.czy.spring",
+        "com.utils.common",
+        "com.utils.redisson",
+        "com.utils.minio",
+        "com.utils.redis",
+        "com.utils.rabbitmq",
+}) // 扫描bean
+@EnableElasticsearchRepositories(basePackages = "com.api.mapper")
+public class PurchaseServiceApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(PurchaseServiceApplication.class)
+                .initializers(new PortApplicationContextInitializer())
+                .run(args);
+    }
+}
+// todo 解决校验不生效问题 test-service
