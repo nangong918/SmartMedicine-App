718e8759dc218857d94091ed8c712af491643a5d
chenzy <1322596900@qq.com>
2025-08-16 18:24:49 +0800

    feat(business): 添加 medicine-service模块
    - 在 business 模块中添加 medicine-service 子模块
    - 新建 MedicineServiceApplication 类作为医学服务的启动类
    - 添加应用配置文件 application.yml
    - 创建 medicine-service 的 Maven 项目对象模型文件 pom.xml
    - 修正 purchase-service 的 artifactId

---
+++ spring-server/business/medicine-service/src/main/java/com/czy/medicine/MedicineServiceApplication.java
@@ -0,0 +1,33 @@
+package com.czy.medicine;
+
+
+import com.utils.common.start.PortApplicationContextInitializer;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+
+@MapperScan({"com.czy.medicine.mapper", "com.utils.minio.mapper"})    // 扫描mapper
+@SpringBootApplication(scanBasePackages = {
+        // 扫描api模块
+        "com.czy.api",
+        // 扫描本模块
+        "com.czy.medicine",
+        // 扫描工具类
+        "com.czy.spring",
+        "com.utils.common",
+        "com.utils.redisson",
+        "com.utils.minio",
+        "com.utils.redis",
+        "com.utils.rabbitmq",
+}) // 扫描bean
+@EnableElasticsearchRepositories(basePackages = "com.api.mapper")
+public class MedicineServiceApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(MedicineServiceApplication.class)
+                .initializers(new PortApplicationContextInitializer())
+                .run(args);
+    }
+}
+// todo 解决校验不生效问题 test-service
