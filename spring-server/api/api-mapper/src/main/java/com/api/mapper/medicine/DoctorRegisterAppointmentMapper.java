aebf5bedc997b8879a2ca732f4dc4fd6d236e890
chenzy <1322596900@qq.com>
2025-08-19 12:00:16 +0800

    feat(api-mapper): 添加医生预约挂号相关查询接口
    - 新增根据 DO 记录获取医生卡片信息列表的查询接口
    - 新增根据参数获取医生预约挂号记录的查询接口
    - 在 XML 文件中添加对应的 SQL 语句

--- spring-server/api/api-mapper/src/main/java/com/api/mapper/medicine/DoctorRegisterAppointmentMapper.java
+++ spring-server/api/api-mapper/src/main/java/com/api/mapper/medicine/DoctorRegisterAppointmentMapper.java
@@ -1,9 +1,12 @@
 package com.api.mapper.medicine;
 
 import com.czy.api.domain.Do.medicine.DoctorRegisterAppointmentDo;
+import com.czy.api.domain.ao.LocationAo;
 import com.czy.api.domain.bo.medicine.RegisterAppointmentDoctorCardBo;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 /**
@@ -12,7 +15,62 @@ import java.util.List;
  */
 @Mapper
 public interface DoctorRegisterAppointmentMapper {
+
+    // 根据do记录获取 cardVo的bo
+
+    /**
+     *
+     * @param dos   doList
+     * @return      boList
+        SELECT
+            dt.avatarFileId as doctorAvatarFileId,
+            dt.name as doctorName,
+            dt.title as doctorTitle,
+
+            ht.name as hospitalName,
+            ht.level as hospitalLevel,
+            ht.province as locationProvince,
+            ht.city as locationCity,
+            ht.region as locationRegion,
+            ht.longitude as longitude,
+            ht.latitude as latitude,
+
+            drat.remainCount as remainCount,
+            drat.cost as cost,
+            drat.beginDate as beginDate,
+            drat.endDate as endDate,
+            drat.status as status
+        FROM doctor_register_appointment AS drat
+        LEFT JOIN doctor AS dt ON drat.doctorId = dt.id
+        LEFT JOIN hospital AS ht ON drat.hospitalId = ht.id
+        WHERE drat.doctorId in (item.doctorId)
+     */
     List<RegisterAppointmentDoctorCardBo> getDoctorCardBosByDos(
             List<DoctorRegisterAppointmentDo> dos
+    );
+
+    /**
+     * 根据参数获取do
+     * @param registerLocation                  地点
+     * @param registerDate                      日期
+     * @param registerDepartmentCode            科室
+     * @param registerSubjectCode               科目
+     * @return  doctorRegisterAppointmentDo
+       SELECT *
+       FROM doctor_register_appointment AS drat
+       LEFT JOIN hospital AS hos ON drat.hospital_id = hos.id
+       WHERE hos.province = #{registerLocation.province}
+       AND hos.city = #{registerLocation.city}
+       AND hos.region = #{registerLocation.region}
+       AND drat.department_id = #{registerDepartmentCode}
+       AND drat.subject_id = #{registerSubjectCode}
+       AND drat.begin_date <= #{registerDate}
+       AND drat.end_date >= #{registerDate}
+     */
+    List<DoctorRegisterAppointmentDo> getDosByParam(
+            @Param("location") LocationAo registerLocation,
+            @Param("date") LocalDateTime registerDate,
+            @Param("departmentCode") Integer registerDepartmentCode,
+            @Param("SubjectCode") Integer registerSubjectCode
     );
 }
