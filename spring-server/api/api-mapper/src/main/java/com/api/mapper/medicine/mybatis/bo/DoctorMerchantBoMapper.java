3ac15d4646237173daba97f349a862045bd218f2
chenzy <1322596900@qq.com>
2025-08-25 12:03:05 +0800

    feat(api-mapper): 增加批量获取医生信息的功能
    - 新增 getDoctorCardBosByUserCustomerAppointmentDos 方法，用于批量获取医生信息
    - 该方法根据用户订单列表查询医生信息，包括医生头像、姓名、职称、医院名称、医院等级、地理位置等
    - 优化了 SQL 查询语句，提高了查询效率
    - 增加了空列表判断，提高了代码健壮性

--- spring-server/api/api-mapper/src/main/java/com/api/mapper/medicine/mybatis/bo/DoctorMerchantBoMapper.java
+++ spring-server/api/api-mapper/src/main/java/com/api/mapper/medicine/mybatis/bo/DoctorMerchantBoMapper.java
@@ -1,6 +1,7 @@
 package com.api.mapper.medicine.mybatis.bo;
 
 import com.czy.api.domain.Do.medicine.DoctorMerchantAppointmentDo;
+import com.czy.api.domain.Do.medicine.UserCustomerAppointmentDo;
 import com.czy.api.domain.bo.medicine.RegisterAppointmentDoctorCardBo;
 import org.apache.ibatis.annotations.Param;
 
@@ -18,34 +19,63 @@ public interface DoctorMerchantBoMapper {
      * @param list   doList
      * @return      boList
     SELECT
-    dt.avatarFileId as doctorAvatarFileId,
-    dt.name as doctorName,
-    dt.title as doctorTitle,
+        dt.avatar_file_id AS doctorAvatarFileId,
+        dt.name AS doctorName,
+        dt.title AS doctorTitle,
 
-    ht.name as hospitalName,
-    ht.level as hospitalLevel,
-    ht.province as locationProvince,
-    ht.city as locationCity,
-    ht.region as locationRegion,
-    ht.longitude as longitude,
-    ht.latitude as latitude,
+        ht.name AS hospitalName,
+        ht.level AS hospitalLevel,
+        ht.province AS locationProvince,
 
-    dma.remainCount as remainCount,
-    dma.cost as cost,
-    dma.beginDate as beginDate,
-    dma.endDate as endDate,
-    dma.status as status
+        ht.city AS locationCity,
+        ht.region AS locationRegion,
+        ht.longitude AS longitude,
+        ht.latitude AS latitude,
+
+        dma.remain_count AS remainCount,
+        dma.cost AS cost,
+        dma.begin_date AS beginDate,
+        dma.end_date AS endDate,
+        dma.id AS doctorMerchantId
     FROM doctor_merchant_appointment AS dma
-    LEFT JOIN doctor AS dt ON dma.doctorId = dt.id
-    LEFT JOIN hospital AS ht ON dma.hospitalId = ht.id
-    WHERE dma.doctorId in (item.doctorId)
+    INNER JOIN doctor AS dt ON dma.doctor_id = dt.id
+    INNER JOIN hospital AS ht ON dma.hospital_id = ht.id
+    WHERE dma.id IN (item.id)
      */
     List<RegisterAppointmentDoctorCardBo> getDoctorCardBosByDoctorMerchantDos(
             @Param("list") List<DoctorMerchantAppointmentDo> list
     );
 
-//    List<RegisterAppointmentDoctorCardBo> getDoctorCardBosByUserCustomerAppointmentDos(
-//            @Param("list") List<UserCustomerAppointmentDo> list
-//    );
+    /**
+     * 批量获取医生信息
+     * @param list  用户订单ids
+     * @return  医生信息
+     SELECT
+        dt.avatar_file_id as doctorAvatarFileId,
+        dt.name as doctorName,
+        dt.title as doctorTitle,
+
+        ht.name as hospitalName,
+        ht.level as hospitalLevel,
+        ht.province as locationProvince,
+        ht.city as locationCity,
+        ht.region as locationRegion,
+        ht.longitude as longitude,
+        ht.latitude as latitude,
+
+        dma.remain_count AS remainCount,
+        dma.cost AS cost,
+        dma.begin_date AS beginDate,
+        dma.end_date AS endDate,
+        dma.id AS doctorMerchantId
+    FROM user_customer_appointment_order AS ucao
+    INNER JOIN doctor_merchant_appointment AS dma ON ucao.doctor_merchant_appointment_id = dma.id
+    INNER JOIN doctor AS dt ON dma.doctor_id = dt.id
+    INNER JOIN hospital AS ht ON dma.hospital_id = ht.id
+    WHERE dma.id in (item.doctorMerchantAppointmentId)
+     */
+    List<RegisterAppointmentDoctorCardBo> getDoctorCardBosByUserCustomerAppointmentDos(
+            @Param("list") List<UserCustomerAppointmentDo> list
+    );
 
 }
