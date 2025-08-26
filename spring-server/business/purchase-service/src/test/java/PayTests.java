dfb49d60b85d84b470281aa7ef0163bbd06d3050
chenzy <1322596900@qq.com>
2025-08-26 15:40:10 +0800

    test(purchase-service): 添加支付相关单元测试
    - 新增 PayTests 类，包含两个测试方法
    - startTest 方法用于检查 orderService 是否正确初始化
    - createOrderTest 方法测试订单创建流程，包括设置订单信息、调用创建订单方法以及线程休眠
    - 使用 @SpringBootTest 注解启动整个应用上下文进行集成测试
    - 引入日志记录，方便调试和查看测试结果

---
+++ spring-server/business/purchase-service/src/test/java/PayTests.java
@@ -0,0 +1,49 @@
+import com.czy.api.constant.UserOrderStatusEnum;
+import com.czy.api.domain.dto.mq.AppointmentOrderDto;
+import com.czy.purchase.PurchaseServiceApplication;
+import com.czy.purchase.service.OrderService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+/**
+ * @author 13225
+ * @date 2025/8/26 15:31
+ */
+@Slf4j
+@SpringBootTest(classes = PurchaseServiceApplication.class)
+@TestPropertySource("classpath:application.yml")
+public class PayTests {
+
+    @Autowired
+    private OrderService orderService;
+
+    @Test
+    public void startTest(){
+        log.info("start test; orderService: {}", orderService);
+    }
+
+    @Test // 成功
+    public void createOrderTest(){
+        AppointmentOrderDto dto = new AppointmentOrderDto();
+        dto.setDoctorMerchantAppointmentId(1L);
+        dto.setUserId(1L);
+        dto.setOrderId(1L);
+        dto.setOrderStatusEnum(UserOrderStatusEnum.WAITING_PAYMENT);
+        // 设置3秒之后就过期
+        dto.setEffectiveTime(3L);
+        dto.setCurrentTime(System.currentTimeMillis());
+
+        orderService.createOrder(dto);
+
+        // 线程等待30秒
+        try {
+            Thread.sleep(30 * 1000L);
+        } catch (Exception e){
+            log.error("线程休眠失败", e);
+        }
+    }
+
+}
