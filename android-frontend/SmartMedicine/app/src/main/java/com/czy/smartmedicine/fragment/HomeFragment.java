9e282361d59ae4ecaa46b1770e8bfe91aacb3f41
czy! <1322596900@qq.com>
2025-08-27 22:10:06 +0800

    feat(appview): 添加首页顶部栏
    - 新增 HomeViewPagerBar 自定义控件
    - 在 HomeFragment 中集成 HomeViewPagerBar
    - 添加相应的字符串资源
    - 调整布局约束以适应新控件

--- android-frontend/SmartMedicine/app/src/main/java/com/czy/smartmedicine/fragment/HomeFragment.java
+++ android-frontend/SmartMedicine/app/src/main/java/com/czy/smartmedicine/fragment/HomeFragment.java
@@ -105,6 +105,10 @@ public class HomeFragment extends BaseFragment<FragmentHomeBinding> {
                 }
             });
         });
+
+        binding.homeTopBar.setOnViewPagerBarClickListener(position -> {
+
+        });
     }
 
     //---------------------------viewModel---------------------------
