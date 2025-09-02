1ed523220b5cfb334e7e6881df479c6737e876f2
czy! <1322596900@qq.com>
2025-09-02 21:58:30 +0800

    refactor(domain): 重构帖子详情页面相关实体类
    - 新增 CommentAo 类，用于帖子评论的数据处理
    - 重构 CommentVo 类，简化字段并优化结构
    - 重命名 PostActivityVo 为 PostAAo，作为帖子详情页面的数据模型
    - 重命名 PostVoLd 为 PostAVo，优化帖子信息的 LiveData 封装

--- android-frontend/SmartMedicine/data/domain/src/main/java/com/czy/domain/fragmentActivityAo/post/PostAAo.java
+++ android-frontend/SmartMedicine/data/domain/src/main/java/com/czy/domain/fragmentActivityAo/post/PostAAo.java
@@ -2,13 +2,18 @@ package com.czy.domain.fragmentActivityAo.post;
 
 import androidx.lifecycle.MutableLiveData;
 
+import com.czy.domain.vo.activity.PostAVo;
 import com.czy.domain.vo.entity.home.CommentVo;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class PostActivityVo {
-    public PostVoLd postVoLd = new PostVoLd();
+public class PostAAo {
+    // view
+    public PostAVo postAVo = new PostAVo();
     public List<CommentVo> commentVos = new ArrayList<>();
     public MutableLiveData<Integer> commentNumLd = new MutableLiveData<>(0);
+
+    // data
+    public Long postId;
 }
---
+++ android-frontend/SmartMedicine/data/domain/src/main/java/com/czy/domain/vo/activity/PostAVo.java
@@ -0,0 +1,33 @@
+package com.czy.domain.vo.activity;
+
+
+import androidx.lifecycle.MutableLiveData;
+
+import com.czy.domain.ao.entity.CommentAo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * postVo的liveData，要与PostVo同步
+ * @see com.czy.domain.vo.entity.home.PostVo
+ */
+public class PostAVo {
+
+    // author
+    public MutableLiveData<String> authorAvatarUrlLd = new MutableLiveData<>("");
+    public MutableLiveData<String> authorNameLd = new MutableLiveData<>("");
+    // post
+    public MutableLiveData<String> postTitleLd = new MutableLiveData<>("");
+    public MutableLiveData<String> postContentLd = new MutableLiveData<>("");
+    public MutableLiveData<String> postPublishTimeLd = new MutableLiveData<>("");
+    public MutableLiveData<String> postViewNumLd = new MutableLiveData<>("");
+    // action
+    public MutableLiveData<Boolean> likePostLd = new MutableLiveData<>(false);
+    public MutableLiveData<Boolean> collectPostLd = new MutableLiveData<>(false);
+    public MutableLiveData<Boolean> dislikePostLd = new MutableLiveData<>(false);
+
+    // comment
+    public List<CommentAo> commentAos = new ArrayList<>();
+    public MutableLiveData<Integer> commentNumLd = new MutableLiveData<>(0);
+}
