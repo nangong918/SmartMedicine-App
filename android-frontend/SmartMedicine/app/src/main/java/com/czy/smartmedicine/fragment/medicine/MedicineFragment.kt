e07e4435c089c94697bda09724cbdfddd639f243
czy! <1322596900@qq.com>
2025-09-04 23:32:18 +0800

    feat(medicine): 初始化 medicine fragment并添加顶部导航栏点击事件- 设置顶部导航栏的点击监听器，实现页面切换功能 - 初始化 ViewModel 并配置 ViewPager2适配器 - 添加 MedicineFAo 并设置初始位置为预约页面

--- android-frontend/SmartMedicine/app/src/main/java/com/czy/smartmedicine/fragment/medicine/MedicineFragment.kt
+++ android-frontend/SmartMedicine/app/src/main/java/com/czy/smartmedicine/fragment/medicine/MedicineFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.czy.appview.view.medicine.MedicineViewPagerEnum
+import com.czy.domain.fragmentActivityAo.medicine.MedicineFAo
 import com.czy.smartmedicine.databinding.FragmentMedicineBinding
 import com.czy.smartmedicine.utils.BaseVmFragment
 import com.czy.smartmedicine.viewModel.fragment.medicine.MedicineVm
@@ -36,10 +38,27 @@ class MedicineFragment : BaseVmFragment<FragmentMedicineBinding, MedicineVm>(
 
     }
 
+    override fun setListener() {
+        super.setListener()
+
+        // 设置顶部导航栏的点击监听器
+        binding.medicineSelectBar.setOnViewPagerBarClickListener { position ->
+            binding.vPager2.setCurrentItem(position, true)
+        }
+    }
+
     //---------------------------ViewModel---------------------------
 
     override fun initViewModel() {
         super.initViewModel()
+
+        val fao = MedicineFAo()
+        fao.currentPosition.value = MedicineViewPagerEnum.APPOINTMENT.index
+
+        vm.init(fao, this)
+
+        // 设置 ViewPager2 的适配器
+        binding.vPager2.adapter = vm.medicineViewPagerAdapter
     }
 
 }
