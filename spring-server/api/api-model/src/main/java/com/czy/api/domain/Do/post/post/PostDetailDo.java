d2853795cf376899abe94f0d85f0b7d2105e15a9
chenzy <1322596900@qq.com>
2025-09-05 11:23:32 +0800

    docs(post): 添加社区系统设计思路文档
    - 新增社区系统设计思路文档，详细说明了发布和获取 post 的模块设计
    - 解释了使用 MongoDB 存储文章信息的原因和优势
    - 提供了 Redis 存储高频变更数据的实现示例
    - 在 PostDetailDo 类中添加了关于使用 MongoDB 的解释注释

--- spring-server/api/api-model/src/main/java/com/czy/api/domain/Do/post/post/PostDetailDo.java
+++ spring-server/api/api-model/src/main/java/com/czy/api/domain/Do/post/post/PostDetailDo.java
@@ -12,8 +12,11 @@ import java.util.List;
  * @date 2025/4/16 21:04
  * 朋友圈Details帖子Do
  * 存放在mongoDB + ES
+ * 我来解释一下为什么要用MongoDB:
+ * MongoDB存储Json格式, 字段比较灵活, 但是代价是JOIN联合查询不能实现.
+ * 此处post查询都是用id去直接获取, 不会出现JOIN
+ * 并且post文章需求很可能出现文章本身内部哪里在哪行是存在图片的这种，mysql是不好实现的, 但是mongodb就好实现.
  */
-
 @org.springframework.data.mongodb.core.mapping.Document("post_detail")
 @Data
 public class PostDetailDo implements Serializable {
