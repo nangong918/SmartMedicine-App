0c3b179e28a0fe53eff97cf4e86fa21d782e2a6e
czy! <1322596900@qq.com>
2025-09-08 00:40:03 +0800

    refactor(order): 重构订单相关 ViewModel 类
    - 为订单相关的 ViewModel 类添加必要的参数和伴生对象
    - 新增 OrderAppointmentAVm、OrderAppointmentFVm、OrderListVm、OrderMainVm、OrderPurchaseAVm 和 OrderPurchaseFVm 类的构造函数
    - 添加 ApiRequestImpl 和 SocketMessageSender 参数，用于网络请求和Socket通信
    - 在每个类中添加 TAG 伴生对象，用于日志打印和调试

--- android-frontend/SmartMedicine/app/src/main/java/com/czy/smartmedicine/viewModel/fragment/order/OrderPurchaseFVm.kt
+++ android-frontend/SmartMedicine/app/src/main/java/com/czy/smartmedicine/viewModel/fragment/order/OrderPurchaseFVm.kt
@@ -1,6 +1,16 @@
 package com.czy.smartmedicine.viewModel.fragment.order
 
 import androidx.lifecycle.ViewModel
+import com.czy.appcore.network.netty.api.send.SocketMessageSender
+import com.czy.dao.networkRepository.ApiRequestImpl
 
-class OrderPurchaseFVm: ViewModel() {
+class OrderPurchaseFVm(
+    private val apiRequestImpl: ApiRequestImpl,
+    private val socketMessageSender: SocketMessageSender
+): ViewModel() {
+
+    companion object {
+        val TAG: String = OrderPurchaseFVm::class.java.name
+    }
+
 }
